aac809f12c74800ab39facd5e11de685b5c18e10
Natithorn <natithorn.s@kkumail.com>
2024-09-16 00:14:54 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,8 +74,6 @@ export default function Home() {
       });
     };
 
-
-
   const handleDeleteTodo = (index: number) => {
     const todoToDelete = todos.at(index);
     if (!todoToDelete || !todoToDelete._id) return;
